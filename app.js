6886bddf6276128befebdd5b8f93d1d09d2b34b7
heimusu <heimusu@gmail.com>
2018-05-07 11:48:35 +0900

    postされたメッセージの暗号化とパスワードとしての比較

--- app.js
+++ app.js
@@ -3,11 +3,33 @@ const oauth = require('oauth')
 const http = require('http')
 const path = require('path')
 const crypto = require('crypto')
+const bodyParser = require('body-parser')
 
 
 const app = express();
 app.use(express.static(__dirname));
 app.set('port', process.env.PORT || 3000);
+
+
+// Cross Originを有効化
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  next();
+});
+
+// Optionsも
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
+
+
+// urlencodeとjsonの初期化
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 
 // 暗号化
@@ -18,21 +40,22 @@ const hashed = password => {
   return value;
 }
 
-
-app.get('/', function(req, res){
-  res.render('index.html');
-});
-
-
-app.get('/login', function(req,res){
-  const text = 'hogehoge'
-  const hashedPassword = hashed(text)
-  const result = {
-    message: 'ok',
-    password: text,
-    hashedPassword: hashedPassword
+app.post('/login', (req, res, next) => {
+  const password = 'hogehoge'
+  const text = req.body.password
+  try {
+    if(hashed(password) === hashed(text)) {
+      res.send(200)
+    }
+    else {
+      res.status(401).json({message: 'パスワードが一致しません'})
+ 
+    }
   }
-  res.json(result);
+  catch (error){
+    res.status(500).json({message: 'error'})
+  }
+
 });
 
 app.listen(3000, function(){
