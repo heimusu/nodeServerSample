7acb161a3b2f76b765170b18762ae3b08f9848ef
heimusu <heimusu@gmail.com>
2018-05-07 11:21:13 +0900

    crypto導入してサンプルパスワードの暗号化をした

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express')
 const oauth = require('oauth')
 const http = require('http')
 const path = require('path')
+const crypto = require('crypto')
 
 
 const app = express();
 app.use(express.static(__dirname));
 app.set('port', process.env.PORT || 3000);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/'));
-app.engine('htm', require('ejs').renderFile);
-app.engine('html', require('ejs').renderFile);
+
+// 暗号化
+const hashed = password => {
+  let hash = crypto.createHmac('sha512', password)
+  hash.update(password)
+  let value = hash.digest('hex')
+  return value;
+}
 
 
 app.get('/', function(req, res){
@@ -20,8 +25,12 @@ app.get('/', function(req, res){
 
 
 app.get('/login', function(req,res){
+  const text = 'hogehoge'
+  const hashedPassword = hashed(text)
   const result = {
-    message: 'ok'
+    message: 'ok',
+    password: text,
+    hashedPassword: hashedPassword
   }
   res.json(result);
 });
